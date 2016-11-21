999332dfff5531b748c4987d78145aff52f4130c
eddydn <eddydn@gmail.com>
2016-11-21 10:35:05 +0700

    First Commit
    First Commit

---
+++ MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace AndroidFullScreen
+{
+    [Activity(Label = "AndroidFullScreen", MainLauncher = true, Icon = "@drawable/icon",
+        Theme ="@android:style/Theme.Holo.Light.NoActionBar.Fullscreen")]
+    public class MainActivity : Activity
+    {
+       
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+                  }
+    }
+}
+
